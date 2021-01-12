c8c9944055501e608abf78d198f09a289acc8270
AnnabelZhang1 <azhang22@stuy.edu>
2021-01-12 12:01:08 -0500

    outline

---
+++ Radix.java
@@ -0,0 +1,24 @@
+public class Radix{
+
+  public static int nth(int n, int col){
+    
+  }
+
+  public static int length(int n){
+
+  }
+
+  public static void merge(MyLinkedList original, MyLinkedList[]buckets)
+
+  public static void main(String[] args){
+    nth(123, 1); //2
+    nth(-123, 1); //2
+    nth(123, 2); //1
+    nth(-123, 2); //1
+
+    length(0); //1
+    length(15); //2
+    length(-10); //2
+    length(5112); //4
+  }
+}
