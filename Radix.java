c48894644509303b3b2898e718f24d8bb4250463
AnnabelZhang1 <azhang22@stuy.edu>
2021-01-13 20:36:28 -0500

    added nth method

--- Radix.java
+++ Radix.java
@@ -1,24 +1,29 @@
 public class Radix{
 
   public static int nth(int n, int col){
-    
+    int numplace = (int)Math.pow(10, col + 1);
+    return Math.abs((n%numplace)/(numplace/10));
   }
-
+/*
   public static int length(int n){
 
   }
 
-  public static void merge(MyLinkedList original, MyLinkedList[]buckets)
+  public static void merge(MyLinkedList original, MyLinkedList[]buckets){
 
+  }
+
+  //public static void merge(SortableLinkedList original, SortableLinkedList[]buckets)
+*/
   public static void main(String[] args){
-    nth(123, 1); //2
-    nth(-123, 1); //2
-    nth(123, 2); //1
-    nth(-123, 2); //1
-
+    System.out.println(nth(123, 1)); //2
+    System.out.println(nth(-123, 1)); //2
+    System.out.println(nth(123, 2)); //1
+    System.out.println(nth(-123, 2)); //1
+/*
     length(0); //1
     length(15); //2
     length(-10); //2
-    length(5112); //4
+    length(5112); //4*/
   }
 }
